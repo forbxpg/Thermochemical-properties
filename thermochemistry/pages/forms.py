c85a205a2f53922a777e13ed590c091e2736851b
forbxpg <dwizard80@gmail.com>
2025-04-02 01:49:16 +0300

    rm useless files

--- thermochemistry/pages/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class SearchForm(forms.Form):
-    name = forms.CharField(label='Name of the substance', max_length=10, required=True)
--- thermochemistry/thermochemistry/urls.py
+++
@@ -1,9 +0,0 @@
-"""URLConf."""
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('pages.urls')),
-    path('', include('elements.urls')),
-    path('admin/', admin.site.urls),
-]
